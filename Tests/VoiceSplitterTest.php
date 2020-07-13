dc61f08138ba3cb42443cff8e528d4646fb047c7
KosukeNaito <tonightoo7110@gmail.com>
2020-07-13 14:44:19 +0900

    test make destination files

--- Tests/VoiceSplitterTest.php
+++ Tests/VoiceSplitterTest.php
@@ -6,13 +6,12 @@ class VoiceSplitterTest extends Testcase {
 
     var $vs;
     
-    public function testInstantiate() {
-        $vs = new VoiceSplitter("..\\targets\\normal.mp3");
-    }
-
     public function testSplit() {
-        $vs.split("..\\targets\\normal\\");
-        $this->assertEquals(file_exists("..\\targets\\normal\\1.mp3"), true);
+        $vs = new VoiceSplitter("./targets/normal.mp3");
+        $vs->split("./targets/normal/");
+
+        $this->assertEquals(file_exists("./targets/normal/"), true);
+        $this->assertEquals(file_exists("./targets/normal/1.mp3"), true);        
     }
 
 }
--- src/VoiceSplitter.php
+++ src/VoiceSplitter.php
@@ -2,8 +2,12 @@
 
 class VoiceSplitter {
 
-    function __construct($filePath) {
-        
+    private $srcPath;
+
+    function __construct($srcPath) {
+        if (file_exists($srcPath)) {
+            $this->srcPath = $srcPath;
+        }
     }
 
     function split($dirPath) {
@@ -11,14 +15,17 @@ class VoiceSplitter {
         //指定ディレクトリがない場合　ディレクトリの作成
         if (!file_exists($dirPath)) {
             mkdir($dirPath, 0777, true);
+            echo 'フォルダが作成されました';
         }
 
         if (file_exists($dirPath)) {
-            
+            $destPath = $dirPath . "1.mp3";
+            if (touch($destPath)) {
+                echo 'ファイルの作成に成功しました';
+            } else {
+                echo 'ファイルの作成に失敗しました';
+            }
         }
-
-        return true;
-
     }
 
     function __destruct() {
@@ -27,4 +34,4 @@ class VoiceSplitter {
 
 }
 
-?>
+?>+
